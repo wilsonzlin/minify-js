3ddcfbdc4e79486ba0dddd524af853dab03e4564
Wilson Lin <code@wilsonl.in>
2022-09-27 23:13:06 +1000

    Fix Node.js library

--- nodejs/src/lib.rs
+++ nodejs/src/lib.rs
@@ -1,15 +1,17 @@
+use std::str::FromStr;
+
 use neon::prelude::*;
 use neon::types::buffer::TypedArray;
 
 fn minify(mut cx: FunctionContext) -> JsResult<JsBuffer> {
-    let top_level_type_raw = cx
-        .argument::<JsString>(0)
-        .and_then(|v| v.to_string(&mut cx))?;
-    let top_level_type = minify_js::TopLevelType::from_str(top_level_type_raw)
-        .map_err(|| cx.type_error("invalid top-level type"))?;
+    let top_level_mode_raw = cx.argument::<JsString>(0).map(|v| v.value(&mut cx))?;
+    let top_level_mode = match minify_js::TopLevelMode::from_str(&top_level_mode_raw) {
+        Ok(m) => m,
+        Err(_) => return cx.throw_type_error("invalid top-level mode"),
+    };
     let src = cx.argument::<JsBuffer>(1)?;
     let mut out = Vec::new();
-    match minify_js::minify(top_level_type, src.as_slice(&mut cx).to_vec(), &mut out) {
+    match minify_js::minify(top_level_mode, src.as_slice(&mut cx).to_vec(), &mut out) {
         Ok(()) => Ok(JsBuffer::external(&mut cx, out)),
         Err(err) => cx.throw_error(format!("{:?}", err)),
     }
